faa012aaf3f5a7456cb23a19158eb792551cf4ed
Sebabebab <sebastian.briglia.smedsrud@nav.no>
2026-06-04 11:13:30 +0200

    Fjern ubrukt mal og temavelger

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
 import '@navikt/ds-css';
 import { MSWProvider } from '@/mocks/MSWProvider';
-import { ThemeProvider } from '@/components/ThemeProvider';
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="nb" suppressHydrationWarning>
+    <html lang="nb">
       <body>
-        <MSWProvider>
-          <ThemeProvider>{children}</ThemeProvider>
-        </MSWProvider>
+        <MSWProvider>{children}</MSWProvider>
       </body>
     </html>
   );
